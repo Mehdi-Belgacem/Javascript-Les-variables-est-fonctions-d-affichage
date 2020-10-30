20386d187447ec791030ae7cd4b3224b16d2be70
Mehdi-Belgacem <72203119+Mehdi-Belgacem@users.noreply.github.com>
2020-10-30 12:18:17 +0100

    maj document

--- exercice3/script.js
+++ exercice3/script.js
@@ -1,5 +1,6 @@
-let name='Mehdi';
+let name='Jean';
+let lastname='Dupont'
 let age= 26;
 let city= 'Noyon';
-alert(`Bonjour, je m'appelle ` + name + ` j'ai ` + age + ` ans et j'habite ` + city)
+alert(`Bonjour, je m'appelle ` + name + " " + lastname + ` j'ai ` + age + ` ans et j'habite ` + city)
 //Pour ajouter un mot avec une apostrophe dans une simple cote il faut utiliser (\) avant (').
